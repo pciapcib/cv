b8db0f12386c46cbeda7b3e7a3f9c16f81fecdb9
Kamisama <pciapcib@163.com>
2016-09-30 18:08:17 +0800

    Done template

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+  #app
+    img(src="./assets/logo.png")
+    h1 {{ msg }}
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'Hello Vue!'
+    }
+  }
+}
+</script>
+
+<style lang="sass">
+  body
+    font-family: Helvetica, sans-serif
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,85 @@
+var path = require('path')
+var webpack = require('webpack')
+
+module.exports = {
+  entry: './src/main.js',
+
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist/',
+    filename: 'build.js'
+  },
+
+  resolveLoader: {
+    root: path.join(__dirname, 'node_modules')
+  },
+
+  module: {
+    preLoaders: [{
+      test: /\.vue$/,
+      loader: 'eslint',
+      exclude: /node_modules/
+    }, {
+      test: /\.js$/,
+      loader: 'eslint',
+      exclude: /node_modules/
+    }],
+
+    loaders: [{
+      test: /\.vue$/,
+      loader: 'vue'
+    }, {
+      test: /\.js$/,
+      loader: 'babel',
+      exclude: /node_modules/
+    }, {
+      test: /\.(png|jpg|gif|svg)$/,
+      loader: 'url',
+      query: {
+        limit: 10000,
+        name: '[name].[hash:7].[ext]'
+      }
+    }]
+  },
+
+  eslint: {
+    formatter: require('eslint-friendly-formatter')
+  },
+
+  plugins: [
+    new webpack.LoaderOptionsPlugin({
+      vue: {
+        loaders: {
+          sass: 'vue-style!css!postcss!sass?indentedSyntax'
+        },
+
+        postcss: [require('autoprefixer')()]
+      }
+    })
+  ],
+
+  devServer: {
+    historyApiFallback: true,
+    noInfo: true
+  },
+
+  devtool: '#eval-source-map'
+}
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports.devtool = '#source-map'
+
+  // http://vue-loader.vuejs.org/en/workflow/production.html
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    })
+  ])
+}
